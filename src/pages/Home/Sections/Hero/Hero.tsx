f46ac01492a0a2c750ea5d872e7960a2e0efd829
Eduardo Freitas Grunitzky <eduardogrunitzky@gmail.com>
2025-04-29 22:52:25 -0300

    add section's

---
+++ src/pages/Home/Sections/Hero/Hero.tsx
@@ -0,0 +1,24 @@
+import { styled } from "@mui/material"
+import Image from "../../../../assets/images/dudu.jpg"
+
+const Hero = () => {
+
+    const StyledHero = styled("div")(()=> ({
+        backgroundColor: "black"
+    }))
+    const StyledIMG = styled("img")(()=> ({
+        width: "20%",
+        borderRadius: "50%"
+    }))
+
+    return (
+        <>
+            <StyledHero>
+                Ola
+                <StyledIMG src={Image} />
+            </StyledHero>
+        </>
+    )
+}
+
+export default Hero
